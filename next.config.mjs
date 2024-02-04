04e585823d3e0fb90d37cf7eab542c351fb2e650
Bernard Adanlessossi-missoh <bernard.adanlessossi@expleogroup.com>
2024-02-04 17:37:06 +0100

    Updated config files

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack(config) {
+    config.experiments = { ...config.experiments, topLevelAwait: true, asyncWebAssembly: true, layers: true };
+    return config;
+  },
+  images: {}
 };
 
 export default nextConfig;
